6205256f594f326fbb5fc5ea4a95c9b320e77b38
Ted Zhu <tedz2usa@gmail.com>
2016-04-21 09:10:18 -0400

    Calculated tornado effect.

--- Weather-Ted-Demo/Assets/MyEmitter.cs
+++ Weather-Ted-Demo/Assets/MyEmitter.cs
@@ -4,27 +4,39 @@ using System.Collections;
 public class MyEmitter : MonoBehaviour {
 
     public GameObject sphere;
+    private int counter = 10;
+    private int numObjects = 50;
+    private int bounds = 20;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
 
-        int numObjects = 20;
-        int bounds = 20;
+        
 
-        for (int i = 0; i < numObjects; i++)
-        {
-            float x = Random.Range(-bounds, bounds);
-            float z = Random.Range(-bounds, bounds);
-            float y = 2;
+        
 
-            Instantiate(sphere.gameObject, new Vector3(x, y, z), Quaternion.identity);
-        }
+        
 
 	
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+
+        counter--;
+        if (counter < 0)
+        {
+            for (int i = 0; i < numObjects; i++)
+            {
+                float x = Random.Range(-bounds, bounds);
+                float z = Random.Range(-bounds, bounds);
+                float y = 2;
+
+                Instantiate(sphere.gameObject, new Vector3(x, y, z), Quaternion.identity);
+            }
+
+            counter = 10;
+        }
+
 	}
 }
---
+++ Weather-Ted-Demo/Assets/MyParticle.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MyParticle : MonoBehaviour {
+
+    private Rigidbody rb;
+
+    public static Vector3 vortextCenter = Vector3.zero;
+    public static float vortexHeight = 10;
+
+    private static int nextID = 1;
+    private int id;
+    private int counter;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+        id = nextID;
+        nextID++;
+        counter = 10;
+    }
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+        Vector3 towardsCenter = Vector3.Normalize(vortextCenter - transform.position);
+
+        Vector3 towardsCenterForce = towardsCenter * 8;
+
+        Vector3 tangential = Vector3.Normalize(Vector3.Cross(towardsCenter, Vector3.up));
+
+        Vector3 tangentialForce = tangential * 2f;
+
+        Vector3 upwardsForce = Vector3.up * Random.Range(11, 40);//Mathf.Abs(10 - transform.position.y);
+
+        
+        Vector3 force = towardsCenterForce + tangentialForce + upwardsForce;
+
+        if (id == 2)
+        {
+            counter--;
+            if (counter < 0)
+            {
+                Debug.Log("towardsCenterForce: " + towardsCenterForce);
+                Debug.Log("tangentialForce: " + tangentialForce);
+                Debug.Log("upwardsForce: " + upwardsForce);
+                Debug.Log("Sum: " + force);
+                counter = 10;
+            }
+            
+        }
+        
+
+        rb.AddForce(force);
+
+    }
+}
