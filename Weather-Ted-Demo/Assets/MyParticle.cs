539e82e77f1913bcf756d2196bb16afd1525756b
Ted Zhu <tedz2usa@gmail.com>
2016-04-21 10:18:22 -0400

    Remove skybox.

--- Weather-Ted-Demo/Assets/MyParticle.cs
+++ Weather-Ted-Demo/Assets/MyParticle.cs
@@ -23,26 +23,12 @@ public class MyParticle : MonoBehaviour {
         counter = 10;
         transform.localScale = Vector3.one * Random.Range(1.5f, 2.5f);
         
-        if (!printed)
-        {
 
-            Debug.Log("print");
-
-            float i = 0;
-            while( i < 200)
-            {
-                float bias = linearBias(i, 0, 200);
-                Debug.Log("bias(" + i + ") = " + bias);
-                i += 0.5f;
-            }
-
-            printed = true;
-
-        }
 
     }
 	
 	// Update is called once per frame
+
 	void FixedUpdate () {
 
         lifetime += Time.fixedDeltaTime;
@@ -59,40 +45,61 @@ public class MyParticle : MonoBehaviour {
 
         Vector3 tangentialForce = tangential * 4f;
 
-        float MylinearBias = linearBias(transform.position.y, 0, 200);
-        Vector3 upwardsForce = Vector3.up * Random.Range(18, 25);// * (MylinearBias + 1f);
+        Vector3 upwardsForce = Vector3.up * Random.Range(18, 25);
 
-
-
-        Vector3 force = towardsCenterForce + tangentialForce + upwardsForce;
-
-        if (id == 2)
-        {
-            counter--;
-            if (counter < 0)
-            {
-                //Debug.Log("towardsCenterForce: " + towardsCenterForce);
-                //Debug.Log("tangentialForce: " + tangentialForce);
-                //Debug.Log("upwardsForce: " + upwardsForce);
-                //Debug.Log("linearBias: " + MylinearBias);
-                //Debug.Log("Sum: " + force);
-                counter = 10;
-            }
-            
-        }
         
-
+        Vector3 force = towardsCenterForce + tangentialForce + upwardsForce;
+        
         rb.AddForce(force);
 
     }
 
-    public static float linearBias(float value, float begin, float end)
-    {
-        float length = (end - begin);
-        float middle = length / 2;
-        float distanceToMiddle = Mathf.Abs(value - middle);
-        return 1 - distanceToMiddle / middle * 0.95f;
-    }
+
 
 
 }
+
+
+
+
+//if (id == 2)
+//{
+//    counter--;
+//    if (counter < 0)
+//    {
+//        //Debug.Log("towardsCenterForce: " + towardsCenterForce);
+//        //Debug.Log("tangentialForce: " + tangentialForce);
+//        //Debug.Log("upwardsForce: " + upwardsForce);
+//        //Debug.Log("linearBias: " + MylinearBias);
+//        //Debug.Log("Sum: " + force);
+//        counter = 10;
+//    }
+
+//}
+
+
+//public static float linearBias(float value, float begin, float end)
+//{
+//    float length = (end - begin);
+//    float middle = length / 2;
+//    float distanceToMiddle = Mathf.Abs(value - middle);
+//    return 1 - distanceToMiddle / middle * 0.95f;
+//}
+
+
+//if (!printed)
+//{
+
+//    Debug.Log("print");
+
+//    float i = 0;
+//    while( i < 200)
+//    {
+//        float bias = linearBias(i, 0, 200);
+//        Debug.Log("bias(" + i + ") = " + bias);
+//        i += 0.5f;
+//    }
+
+//    printed = true;
+
+//}
